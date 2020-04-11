21255c4e45b94de19ab4d61145cebbda9c0d88fe
Gaëtan Puget <gpuget@excilys.com>
2020-04-11 14:16:19 +0200

    Refacto with private methods

--- src/main/java/com/excilys/StringCalculator.java
+++ src/main/java/com/excilys/StringCalculator.java
@@ -1,6 +1,5 @@
 package com.excilys;
 
-import java.util.function.Supplier;
 import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
@@ -9,9 +8,9 @@ import java.util.stream.IntStream;
  * The type String calculator.
  */
 public final class StringCalculator {
+    private static final Pattern DELIMITER_PATTERN = Pattern.compile("\\n");
     private static final String DEFAULT_NUMBER_DELIMITER = "[,\\n]";
     private static final String DELIMITER_PREFIX = "//";
-    private static final String DELILITER_DELIMITER = "\\n";
     private static final int MAX = 1000;
 
     private StringCalculator() {
@@ -31,29 +30,46 @@ public final class StringCalculator {
             return 0;
         }
 
-        String delimiter;
-        String values;
-        if (input.startsWith(DELIMITER_PREFIX)) {
-            var split = input.substring(DELIMITER_PREFIX.length()).split(DELILITER_DELIMITER, 2);
+        String delimiter = DEFAULT_NUMBER_DELIMITER;
+        if (hasCustomDelimiter(input)) {
+            var split = DELIMITER_PATTERN.split(input.substring(DELIMITER_PREFIX.length()), 2);
             delimiter = split[0];
-            values = split[1];
-        } else {
-            delimiter = DEFAULT_NUMBER_DELIMITER;
-            values = input;
+            input = split[1];
         }
 
+        return add(input, delimiter);
+    }
+
+    private static int add(String input, String delimiter) {
+        check(input, delimiter);
+        return asIntStream(input, delimiter).filter(i -> i < MAX).sum();
+    }
+
+    private static boolean hasCustomDelimiter(String input) {
+        return input.startsWith(DELIMITER_PREFIX);
+    }
+
+    public static void check(String input, String delimiter) {
+        checkFormat(input);
+        checkNoNegativeNumber(input, delimiter);
+    }
+
+    private static void checkFormat(String input) {
         if (input.endsWith("\n")) {
             throw new IllegalArgumentException("missing number");
         }
+    }
 
-        Supplier<IntStream> numbers =
-                () -> Pattern.compile(delimiter).splitAsStream(values).mapToInt(Integer::parseInt);
-        String negativeNumbers =
-                numbers.get().filter(i -> i < 0).mapToObj(String::valueOf).collect(Collectors.joining(", "));
+    private static void checkNoNegativeNumber(String input, String delimiter) {
+        String negativeNumbers = asIntStream(input, delimiter).filter(i -> i < 0)
+                                                              .mapToObj(String::valueOf)
+                                                              .collect(Collectors.joining(", "));
         if (!negativeNumbers.isEmpty()) {
             throw new IllegalArgumentException("not allowed negative number: " + negativeNumbers);
         }
+    }
 
-        return numbers.get().filter(i -> i < MAX).sum();
+    private static IntStream asIntStream(String input, String delimiter) {
+        return Pattern.compile(delimiter).splitAsStream(input).mapToInt(Integer::parseInt);
     }
 }
