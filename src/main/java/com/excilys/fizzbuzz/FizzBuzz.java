4169fa3861e2cdf74f9b0adfa132b40ecc47a117
Gaetan Puget <gaetan.puget@eileo.com>
2020-04-12 10:15:13 +0200

    Naming Test Methods and How to write a Test

--- src/main/java/com/excilys/fizzbuzz/FizzBuzz.java
+++ src/main/java/com/excilys/fizzbuzz/FizzBuzz.java
@@ -5,7 +5,7 @@ public class FizzBuzz {
         throw new AssertionError("Fly you fool !");
     }
 
-    public static String fizzBuzz(int number) {
+    public static String execute(int number) {
         return "";
     }
 }
--- src/test/java/com/excilys/fizzbuzz/FizzBuzzTest.java
+++ src/test/java/com/excilys/fizzbuzz/FizzBuzzTest.java
@@ -1,5 +1,16 @@
 package com.excilys.fizzbuzz;
 
-class FizzBuzzTest {
+import static org.assertj.core.api.Assertions.assertThat;
 
+import org.junit.jupiter.api.Test;
+
+class FizzBuzzTest {
+    @Test
+    public void given1_whenFizzBuzz_then1() {
+        givenNumber_whenFizzBuzz_thenExpected(1, "1");
+    }
+
+    private void givenNumber_whenFizzBuzz_thenExpected(int number, String expected) {
+        assertThat(FizzBuzz.execute(number)).isEqualTo(expected);
+    }
 }
