ee62b6a3ee2014fae487774f430a0bdfe88652a0
Gaetan Puget <gaetan.puget@eileo.com>
2020-04-12 10:56:40 +0200

    Javadoc

--- src/main/java/com/excilys/fizzbuzz/FizzBuzz.java
+++ src/main/java/com/excilys/fizzbuzz/FizzBuzz.java
@@ -1,10 +1,27 @@
 package com.excilys.fizzbuzz;
 
+/**
+ * FizzBuzz tools.
+ */
 public class FizzBuzz {
     private FizzBuzz() {
         throw new AssertionError("Fly you fool !");
     }
 
+    /**
+     * Converts the number:
+     * <ul>
+     *     <li>"Fizz" for number divisible by 3</li>
+     *     <li>"Buzz" for number divisible by 5</li>
+     *     <li>"FizzBuzz" for number divisible by 3 and divisible by 5</li>
+     *     <li>The number itself otherwise</li>
+     * </ul>
+     *
+     * @param number
+     *         the number
+     *
+     * @return the string
+     */
     public static String execute(int number) {
         StringBuilder builder = new StringBuilder();
 
