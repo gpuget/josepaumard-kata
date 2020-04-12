1284a21545b165f4a32cc4e2f631eafee3a9a250
Gaetan Puget <gpuget@excilys.com>
2020-04-12 12:40:43 +0200

    Common year

---
+++ src/main/java/com/excilys/calculator/LeapYearsCalculator.java
@@ -0,0 +1,11 @@
+package com.excilys.calculator;
+
+public class LeapYearsCalculator {
+    private LeapYearsCalculator() {
+        throw new AssertionError("RUN AWAAAY !");
+    }
+
+    public static boolean execute(int year) {
+        return false;
+    }
+}
---
+++ src/test/java/com/excilys/calculator/LeapYearsCalculatorTest.java
@@ -0,0 +1,24 @@
+package com.excilys.calculator;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.Test;
+
+class LeapYearsCalculatorTest {
+    private void givenYear_whenLeapYears_then(int year, boolean leap) {
+        assertThat(LeapYearsCalculator.execute(year)).isEqualTo(leap);
+    }
+
+    private void givenYear_whenLeapYears_thenCommon(int year) {
+        givenYear_whenLeapYears_then(year, false);
+    }
+
+    private void givenYear_whenLeapYears_thenLeap(int year) {
+        givenYear_whenLeapYears_then(year, true);
+    }
+
+    @Test
+    public void given2001_whenLeapYears_thenCommon() {
+        givenYear_whenLeapYears_thenCommon(2001);
+    }
+}
