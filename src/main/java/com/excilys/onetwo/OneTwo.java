a8df8bf3872525dd7527de73400b0b38008f5dd9
Gaetan Puget <gpuget@excilys.com>
2020-04-13 16:16:46 +0200

    Part 2

--- src/main/java/com/excilys/onetwo/OneTwo.java
+++ src/main/java/com/excilys/onetwo/OneTwo.java
@@ -1,19 +1,30 @@
 package com.excilys.onetwo;
 
+import java.util.ArrayDeque;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Deque;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.StringJoiner;
 import java.util.regex.Pattern;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 /**
  * OneTwo string describer.
  */
 public class OneTwo {
-    private static final Map<Integer, String> MAPPING =
+    private static final Map<Integer, String> TO_LETTER =
             Map.of(1, "one", 2, "two", 3, "three", 4, "four", 5, "five", 6, "six", 7, "seven", 8, "eight", 9, "nine");
+    private static final Map<String, Integer> TO_DIGIT;
     private static final String DELIMITER = " ";
+
+    static {
+        TO_DIGIT = Collections.unmodifiableMap(
+                TO_LETTER.entrySet().stream().collect(Collectors.toMap(Map.Entry::getValue, Map.Entry::getKey)));
+    }
 
     private OneTwo() {
         throw new AssertionError("You shall not pass !");
@@ -35,19 +46,39 @@ public class OneTwo {
         StringJoiner joiner = new StringJoiner(DELIMITER);
         Pattern.compile(DELIMITER)
                .splitAsStream(input)
-               .collect(Collectors.groupingBy(s -> MAPPING.get(Integer.parseInt(s)), LinkedHashMap::new,
+               .collect(Collectors.groupingBy(s -> TO_LETTER.get(Integer.parseInt(s)), LinkedHashMap::new,
                                               Collectors.toList()))
-               .forEach((s, list) -> doDescribe(joiner, s, list));
+               .forEach((s, list) -> doRead(joiner, s, list));
         return joiner.toString();
     }
 
-    private static void doDescribe(StringJoiner joiner, String key, List<String> values) {
-        int limit = MAPPING.size();
+    private static void doRead(StringJoiner joiner, String key, List<String> values) {
+        int limit = TO_LETTER.size();
         int number = values.size();
-        if (values.size() > MAPPING.size()) {
-            joiner.add(MAPPING.get(limit)).add(key);
+        if (values.size() > TO_LETTER.size()) {
+            joiner.add(TO_LETTER.get(limit)).add(key);
             number -= limit;
         }
-        joiner.add(MAPPING.get(number)).add(key);
+        joiner.add(TO_LETTER.get(number)).add(key);
+    }
+
+    public static String write(String input) {
+        if (input.isBlank()) {
+            return input;
+        }
+
+        Deque<String> deque = new ArrayDeque<>(Arrays.asList(input.split(DELIMITER)));
+        StringJoiner joiner = new StringJoiner(DELIMITER);
+        do {
+            joiner.add(doWrite(deque.pollFirst(), deque.pollFirst()));
+        } while (!deque.isEmpty());
+        return joiner.toString();
+    }
+
+    private static String doWrite(String number, String value) {
+        return Stream.generate(() -> TO_DIGIT.get(value))
+                     .limit(TO_DIGIT.get(number))
+                     .map(String::valueOf)
+                     .collect(Collectors.joining(DELIMITER));
     }
 }
--- src/test/java/com/excilys/onetwo/OneTwoTest.java
+++ src/test/java/com/excilys/onetwo/OneTwoTest.java
@@ -9,6 +9,10 @@ class OneTwoTest {
         assertThat(OneTwo.read(input)).isEqualTo(output);
     }
 
+    private void givenInput_whenWrite_thenOutput(String input, String output) {
+        assertThat(OneTwo.write(input)).isEqualTo(output);
+    }
+
     @Test
     public void givenEmpty_whenRead_thenEmpty() {
         givenInput_whenRead_thenOutput("", "");
@@ -40,12 +44,47 @@ class OneTwoTest {
     }
 
     @Test
-    public void given244466666_whenRead_thenSevenOne() {
+    public void given244466666_whenRead_thenOneTwoThreeFourFive() {
         givenInput_whenRead_thenOutput("2 4 4 4 6 6 6 6 6", "one two three four five six");
     }
 
     @Test
     public void given555555555555_whenRead_thenNineFiveThreeFive() {
         givenInput_whenRead_thenOutput("5 5 5 5 5 5 5 5 5 5 5 5", "nine five three five");
+    }
+
+    @Test
+    public void givenEmpty_whenWrite_thenEmpty() {
+        givenInput_whenWrite_thenOutput("", "");
+    }
+
+    @Test
+    public void givenOneTwo_whenWrite_then2() {
+        givenInput_whenWrite_thenOutput("one two", "2");
+    }
+
+    @Test
+    public void givenOneOneOneTwo_whenWrite_then12() {
+        givenInput_whenWrite_thenOutput("one one one two", "1 2");
+    }
+
+    @Test
+    public void givenTwoTwo_whenWrite_then22() {
+        givenInput_whenWrite_thenOutput("two two", "2 2");
+    }
+
+    @Test
+    public void givenOneThreeThreeNineTwoEight_whenWrite_then399988() {
+        givenInput_whenWrite_thenOutput("one three three nine two eight", "3 9 9 9 8 8");
+    }
+
+    @Test
+    public void givenOneTwoThreeFourFiveSix_whenWrite_then244466666() {
+        givenInput_whenWrite_thenOutput("one two three four five six", "2 4 4 4 6 6 6 6 6");
+    }
+
+    @Test
+    public void givenNineFiveThreeFive_whenWrite_then555555555555() {
+        givenInput_whenWrite_thenOutput("nine five three five", "5 5 5 5 5 5 5 5 5 5 5 5");
     }
 }
