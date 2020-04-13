bb0cf58cd25652f111314841b179616b6ccdb249
Gaetan Puget <gpuget@excilys.com>
2020-04-13 10:39:22 +0200

    Empty case

---
+++ src/main/java/com/excilys/onetwo/OneTwo.java
@@ -0,0 +1,11 @@
+package com.excilys.onetwo;
+
+public class OneTwo {
+    private OneTwo() {
+        throw new AssertionError("You shall not pass !");
+    }
+
+    public static String describe(String input) {
+        return input;
+    }
+}
---
+++ src/test/java/com/excilys/onetwo/OneTwoTest.java
@@ -0,0 +1,16 @@
+package com.excilys.onetwo;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.Test;
+
+class OneTwoTest {
+    private void givenInput_whenDescribe_thenOutput(String input, String output) {
+        assertThat(OneTwo.describe(input)).isEqualTo(output);
+    }
+
+    @Test
+    public void givenEmpty_whenDescribe_thenEmpty() {
+        givenInput_whenDescribe_thenOutput("", "");
+    }
+}
