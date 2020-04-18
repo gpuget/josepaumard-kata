60bc9ed0ca562418226e5113a878c06cfe41dc54
Gaetan Puget <gpuget@excilys.com>
2020-04-18 14:39:06 +0200

    While method

--- src/main/java/com/excilys/calculator/palindrome/PalindromeCalculator.java
+++ src/main/java/com/excilys/calculator/palindrome/PalindromeCalculator.java
@@ -1,5 +1,7 @@
 package com.excilys.calculator.palindrome;
 
+import java.util.Objects;
+
 public class PalindromeCalculator {
     private PalindromeCalculator() {
         throw new AssertionError("I am not bad I'm just drawn that way");
@@ -9,15 +11,30 @@ public class PalindromeCalculator {
         int length = input.length();
         if (length <= 1) {
             return true;
-        } else if (length == 2) {
-            return input.charAt(0) == input.charAt(length - 1);
         }
 
-        return stringBuilderSolution(input);
+        return whileSolution(input.replace(".", ""));
+    }
+
+    private static boolean whileSolution(String input) {
+        int first = 0;
+        int last = input.length() - 1;
+
+        while (first < last) {
+            if (!Objects.equals(Character.toLowerCase(Character.toLowerCase(input.charAt(first))),
+                                Character.toLowerCase(input.charAt(last)))) {
+                return false;
+            }
+
+            first++;
+            last--;
+        }
+
+        return true;
     }
 
     private static boolean stringBuilderSolution(String input) {
-        String lower = input.toLowerCase().replace(".", "");
+        String lower = input.toLowerCase();
         return new StringBuilder(lower).reverse().toString().equals(lower);
     }
 }
